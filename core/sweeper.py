4492f379ebee810de11916e5b22e47448d1ff867
taserbeat <taserbeat@gmail.com>
2022-10-07 23:01:32 +0900

    refactor: Sweeperの座標を中心点に変更

--- core/sweeper.py
+++ core/sweeper.py
@@ -5,16 +5,16 @@ import typing as t
 class Sweeper:
     def __init__(
         self,
-        x_min: int = 200,
-        y_min: int = 300,
+        x: int = 200,
+        y: int = 300,
         width: int = 50,
         height: int = 10,
         x_velocity: int = 2,
         y_velocity: int = 2,
         color: t.Tuple[int, int, int] = (255, 0, 0)
     ) -> None:
-        self.x_min = x_min
-        self.y_min = y_min
+        self.x = x
+        self.y = y
         self.width = width
         self.height = height
         self.x_velocity = x_velocity
@@ -28,34 +28,37 @@ class Sweeper:
 
         # 左キーの移動
         if pressed_key[pygame.K_LEFT]:
-            self.x_min -= self.x_velocity
+            self.x -= self.x_velocity
         # 右キーの移動
         elif pressed_key[pygame.K_RIGHT]:
-            self.x_min += self.x_velocity
+            self.x += self.x_velocity
         # 上キーの移動
         elif pressed_key[pygame.K_UP]:
-            self.y_min -= self.x_velocity
+            self.y -= self.x_velocity
         # 下キーの移動
         elif pressed_key[pygame.K_DOWN]:
-            self.y_min += self.y_velocity
+            self.y += self.y_velocity
 
         surface_rect = surface.get_bounding_rect()
 
         # 左の移動制限
-        if self.x_min < surface_rect.left:
-            self.x_min = surface_rect.left
+        if self.x - self.width / 2 < surface_rect.left:
+            self.x = surface_rect.left * self.width / 2
 
         # 右の移動制限
-        if self.x_min + self.width > surface_rect.right:
-            self.x_min = surface_rect.right - self.width
+        if self.x + self.width / 2 > surface_rect.right:
+            self.x = surface_rect.right - self.width / 2
 
         # 上の移動制限
-        if self.y_min < surface_rect.top:
-            self.y_min = surface_rect.top
+        if self.y - self.height / 2 < surface_rect.top:
+            self.y = surface_rect.top + self.height / 2
 
         # 下の移動制限
-        if self.y_min + self.height > surface_rect.bottom:
-            self.y_min = surface_rect.bottom - self.height
+        if self.y + self.height / 2 > surface_rect.bottom:
+            self.y = surface_rect.bottom - self.height / 2
 
-        pygame.draw.rect(surface, self.color, (self.x_min, self.y_min, self.width, self.height))
+        x_min = int(self.x - self.width / 2)
+        y_min = int(self.y - self.height / 2)
+
+        pygame.draw.rect(surface, self.color, (x_min, y_min, self.width, self.height))
         return
